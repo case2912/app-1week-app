540871b44e9c82943ce8dd341c6d5f722e1b0a0d
case2912 <shiroyama@maia.eonet.ne.jp>
2019-06-25 11:49:12 +0900

    ctlabelの修正

--- ios-example/CTLabel/CTLabel.swift
+++ ios-example/CTLabel/CTLabel.swift
@@ -14,16 +14,17 @@ class CTLabel: UILabel {
             NSAttributedString.Key.verticalGlyphForm: true,
         ]
         var astr = NSAttributedString(string: self.text ?? "", attributes: stringAttrs)
-        //labelの横幅と文字列の横幅を比較して、labelの文字の大きさをラベルの大きさまで合わせる このときastrのheightは実際のwidth(縦書きのため)
-        var fontSize: CGFloat = 10
-        while rect.width > astr.size().height {
-            let font = UIFont.systemFont(ofSize: fontSize)
+        //labelの横幅と文字列の横幅を比較して、labelの文字の大きさをラベルの大きさまで合わせる fontSizeとrectWidthで比較しないと何故か表示されない。。。
+        var fontSize: CGFloat = 1
+        while rect.width > fontSize {
+            let fontRef = CTFontCreateWithName("AoyagiSosekiFont2OTF" as CFString, fontSize, nil)
             stringAttrs = [
-                NSAttributedString.Key.font: font,
+                kCTFontAttributeName as NSAttributedString.Key: fontRef,
                 NSAttributedString.Key.verticalGlyphForm: true,
             ]
             astr = NSAttributedString(string: self.text ?? "", attributes: stringAttrs)
             fontSize += 1
+
         }
         let frameSetter = CTFramesetterCreateWithAttributedString(astr)
         let newRect = CGRect(x: 2 * rect.origin.x, y: 2 * rect.origin.y, width: rect.width, height: rect.height)
